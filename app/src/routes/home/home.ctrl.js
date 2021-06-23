194a79465f9c1a7188d63138e6041ed049dcb93e
TaeSeok <hant616@gamil.com>
2021-06-23 17:39:04 +0900

    회원가입 라우팅 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -22,10 +22,14 @@ const process = {
   login: (req, res) => {
     const user = new User(req.body);
     const response = user.login();
-    console.log(response);
-
     return res.json(response);
   },
+
+  register: (req, res) => {
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+  }
 };
 
 module.exports = {
