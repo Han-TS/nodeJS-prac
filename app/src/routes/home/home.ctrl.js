bf41741ffcd167a7bdcbc8c27282fadfbcef1712
TaeSeok <hant616@gamil.com>
2021-06-23 16:31:40 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -11,35 +11,20 @@ const output = {
   login: (req, res) => {
     res.render('home/login');
   },
+
+  register: (req, res) => {
+    res.render('home/register');
+  }
 };
 
 // 로그인 검증 ===========================================================
 const process = {
-
   login: (req, res) => {
     const user = new User(req.body);
     const response = user.login();
     console.log(response);
 
-    // const id = req.body.id;
-    // const psword = req.body.psword;
-
-    // const users = UserStorage.getUsers("id", "psword");
-
-    // const response = {};
-    // if (users.id.includes(id)) {
-    //   const idx = users.id.indexOf(id);
-    //   if (users.psword[idx] === psword) {
-    //     response.success = true;
-    //     return res.json(response);
-    //   }
-    // }
-
-    // response.success = false;
-    // response.msg = "로그인에 실패하였습니다."
-
     return res.json(response);
-
   },
 };
 
