4fe5cf701bd312b02d23489d8a027ddcdb91f67c
TaeSeok <hant616@gamil.com>
2021-06-24 11:09:04 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,9 +19,9 @@ const output = {
 
 // 로그인 검증 ===========================================================
 const process = {
-  login: (req, res) => {
+  login: async (req, res) => {
     const user = new User(req.body);
-    const response = user.login();
+    const response = await user.login();
     return res.json(response);
   },
 
